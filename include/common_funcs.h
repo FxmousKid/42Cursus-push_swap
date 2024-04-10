e2efdd9d2f5aa697739dff2533e0436cfca10d14
FxmousKid <iyan005@outlook.com>
2024-04-10 15:36:47 +0200

    Added rotate function in header

--- include/common_funcs.h
+++ include/common_funcs.h
@@ -6,7 +6,7 @@
 /*   By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 19:27:33 by inazaria          #+#    #+#             */
-/*   Updated: 2024/04/10 02:48:22 by inazaria         ###   ########.fr       */
+/*   Updated: 2024/04/10 15:08:03 by inazaria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,11 +28,16 @@ int		*return_int_stack(int argc, char *argv[]);
 char	**parse_stack_input(int argc, char *argv[]);
 int		get_stack_len(int argc, char *argv[]);
 void	free_stack(t_stack *stack);
+void	display_stack(t_stack *stack);
 
 t_stack	*make_stack_a(int argc, char *argv[]);
 t_stack	*make_stack_b(int len_stack_a);
 
 // Moveset functions
+void	ra(t_stack *stack_a);
+void	rb(t_stack *stack_b);
+void	rr(t_stack *stack_a, t_stack *stack_b);
+
 
 // Helper functions
 int		count_occ(char *str, char *charset);
