9807ba3d5c2702efaf27d1a64004696ca5eb55fe
FxmousKid <iyan005@outlook.com>
2024-04-13 17:57:20 +0200

    header up to date

--- include/common_funcs.h
+++ include/common_funcs.h
@@ -6,7 +6,7 @@
 /*   By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 19:27:33 by inazaria          #+#    #+#             */
-/*   Updated: 2024/04/11 15:50:19 by inazaria         ###   ########.fr       */
+/*   Updated: 2024/04/13 00:41:38 by inazaria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,16 @@ typedef struct s_stack
 	int	length;
 }				t_stack;
 
+typedef struct s_elem
+{
+	int	value;
+	int	index;
+}				t_elem;	
+
 // Manipulating the Stack itself
-int		verify_input(char *input);
-void	free_stack_splited_input(char *stack[], int stack_len);
+int		verify_input(int argc, char *argv[]);
+int		verify_dups_in_input(t_stack *stack);
 int		*return_int_stack(int argc, char *argv[]);
-char	**parse_stack_input(int argc, char *argv[]);
 int		get_stack_len(int argc, char *argv[]);
 void	free_stack(t_stack *stack);
 void	display_stack(t_stack *stack);
@@ -35,6 +40,11 @@ t_stack	*make_stack_a(int argc, char *argv[]);
 t_stack	*make_stack_b(int len_stack_a);
 
 // Moveset functions
+void	push(t_stack *from, t_stack *to);
+void	reverse_rotate(t_stack *stack);
+void	rotate(t_stack *stack);
+void	swap(t_stack *stack);
+
 void	ra(t_stack *stack_a);
 void	rb(t_stack *stack_b);
 void	rr(t_stack *stack_a, t_stack *stack_b);
@@ -47,7 +57,27 @@ void	sa(t_stack *stack_a);
 void	sb(t_stack *stack_b);
 void	ss(t_stack *stack_a, t_stack *stack_b);
 
+void	rra_no_print(t_stack *stack_a);
+void	rrb_no_print(t_stack *stack_b);
+void	rrr_no_print(t_stack *stack_a, t_stack *stack_b);
+void	ra_no_print(t_stack *stack_a);
+void	rb_no_print(t_stack *stack_b);
+void	rr_no_print(t_stack *stack_a, t_stack *stack_b);
+void	pa_no_print(t_stack *stack_a, t_stack *stack_b);
+void	pb_no_print(t_stack *stack_a, t_stack *stack_b);
+void	sa_no_print(t_stack *stack_a);
+void	sb_no_print(t_stack *stack_b);
+void	ss_no_print(t_stack *stack_a, t_stack *stack_b);
+
+
 // Helper functions
 int		count_occ(char *str, char *charset);
+
+// Sorting functions
+int		get_min_depth(t_stack *stack);
+void	push_min_top(t_stack *stack);
+void	push_all_from_to(t_stack *from, t_stack *to);
+void	make_b_sorted(t_stack *stack_a, t_stack *stack_b);
+void	stupid_sort(t_stack *stack_a, t_stack *stack_b);
 
 #endif
